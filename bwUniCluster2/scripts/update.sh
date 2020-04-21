425735ad001899a814c947f66a372cbff4128467
Alexandru Saramet <alexandru@saramet.eu>
2020-04-21 22:46:25 +0200

    update _data

--- bwUniCluster2/scripts/update.sh
+++ bwUniCluster2/scripts/update.sh
@@ -8,6 +8,10 @@ A_DIR=${MD}/src/app
 create_data_files()
 {
   ${S_DIR}/collect_data.sh
+
+  DATA_FOLDER="${A_DIR}/_data"
+  [[ ! -d ${DATA_FOLDER} ]] && mkdir -p ${DATA_FOLDER}
+  
   ${S_DIR}/write_bwUniData.sh
   ${S_DIR}/write_data_year.sh
   ${S_DIR}/write_total.sh
@@ -17,3 +21,5 @@ update()
 {
   create_data_files
 }
+
+update
